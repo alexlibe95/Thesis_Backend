86702b27e7260b8e61bf3a1254326f20839aaad0
alexlibe95 <alexlibe95@gmail.com>
2019-01-04 23:38:26 +0200

    adding files

--- users/user.service.js
+++ users/user.service.js
@@ -1,5 +1,4 @@
 ﻿// users hardcoded for simplicity, store in a db for production applications
-var pool = require('../db')
 const getuser = require("../server");
 
 
@@ -8,8 +7,6 @@ const users = [
   { id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' },
   { id: 2, username: 'test2', password: 'test2', firstName: 'Tdfdst', lastName: 'Ufffser' },
   { id: 3, username: 'test3', password: 'tes3', firstName: 'Tefst', lastName: 'Uddddddddser' }
-
-
 ];
 
 module.exports = {
@@ -18,20 +15,15 @@ module.exports = {
 };
 
 async function authenticate({ username, password }) {
-        let use = getuser.getusers();
-
-        use.then(function(results) {
+        let us = getuser.getusers();
+        us.then(function(results){
           console.log(results[1])
-          const user = results.find(u => u.username === username && u.password === password);
-          console.log("Dfdf")
+          const user = users.find(u => u.username === username && u.password === password);
         })
-        if (user) {
+        if(user) {
             const { password, ...userWithoutPassword } = user;
             return userWithoutPassword;
         }
-
-
-
 }
 
 async function getAll() {
